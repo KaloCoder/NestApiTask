340261fb90aa553ea054fd55493bae59c0e219c2
Solo-Cat <sir.noen@yandex.ru>
2023-12-29 11:41:35 +0300

    create app

---
+++ src/task/task.service.ts
@@ -0,0 +1,43 @@
+/* eslint-disable prettier/prettier */
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { TaskDto } from "./task.dto";
+import { PrismaService } from "../prisma.service";
+
+@Injectable()
+export class TaskService {
+  constructor(prisma: PrismaService) {}
+
+  async getById(id: string) {
+    const task = await this.prisma.task.findUnique({
+      where: { id: +id },
+    });
+
+    if (!task) {
+        throw new NotFoundException('Task not found');
+    }
+    return task
+  }
+
+  getAll() {
+    return this.prisma.task.findMany();
+  }
+
+  create(dto: TaskDto) {
+    return this.prisma.task.create({
+      data: dto,
+    });
+  }
+
+  async toggleDone(id: string) {
+    const task = await this.getById(id)
+
+    return this.prisma.task.update({
+        where: {
+            id: task.id
+        },
+        data: {
+            isDone: !task.isDone
+        }
+    })
+  }
+}
